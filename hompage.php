6543139fa703e2c9ffc23c61f9d3fd661625a56f
Devix11 <davideagostini05@gmail.com>
2024-01-09 11:43:37 +0100

    Update hompage.php

---
+++ hompage.php
@@ -0,0 +1,16 @@
+<?php
+// FILEPATH: /Users/dade/Documents/GitHub/BibliotecaPhp/homepage.php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Benvenuto</title>
+</head>
+<body>
+    <h1>Benvenuto</h1>
+    <button onclick="location.href='signup.php'">Registrati</button>
+    <button onclick="location.href='login.php'">Accedi</button>
+</body>
+</html>
