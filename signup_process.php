1b0abda13f0be9d4685ce3ca0d791757e9d09324
Sorgattino <sorgatochristian@gmail.com>
2024-01-10 11:59:05 +0100

    Update

--- signup_process.php
+++ signup_process.php
@@ -9,7 +9,7 @@
 <button onclick="location.href='homepage.php'">Go back to homepage</button>
 <?php
             // Stabilire la connessione al database
-            $db = mysqli_connect('localhosy:3306', 'phpmyadmin', 'ciaone11!', 'biblioteca');
+            $db = mysqli_connect('localhost:3306', 'phpmyadmin', 'ciaone11!', 'biblioteca');
             // Verificare la connessione
             if (!$db) {
                 die("Connessione fallita: " . mysqli_connect_error());
@@ -25,16 +25,24 @@
                     //die è una funzione che termina l'esecuzione dello script
                     die("Errore: dati mancanti");
 
-                $query = "SELECT id FROM utenti_registrati WHERE email = '$email';"
-                if (mysqli_query($db, $query) != NULL){
+                $query = "SELECT * FROM utenti_registrati WHERE email = '$email';"
+                if (mysqli_query($db, $query) != false){
                     die("Errore: Utente gia' esistente!");
                 }
             }
             //Values security-check
-            $name = htmlentities($_POST['name']);
-            $surname = htmlentities($_POST['surname']);
-            $email = htmlentities($_POST['email']);
-            $password = htmlentities($_POST['password']);
+            $nm = htmlentities($_POST['name']);
+            $name = Strip_tags($nm);
+
+            $surn = htmlentities($_POST['surname']);
+            $surname = Strip_tags($surn);
+
+            $ma = htmlentities($_POST['email']);
+            $email = Strip_tags($ma);
+
+            $pass = htmlentities($_POST['password']);
+            $password = Strip_tags($pass);
+
             $timestamp = time();
             
             //Password to hash
