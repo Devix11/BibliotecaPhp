6d608a0a5a7ffaea7914c92506de84aab734ae1f
Devix11 <davideagostini05@gmail.com>
2024-01-23 08:58:50 +0100

    update

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -95,31 +95,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </tr>
             <?php foreach ($books as $book) { ?>
                 <tr>
-                    <form method="POST" action="">
-                        <td><?php echo $book['id']; ?></td>
-                        <td><input type="text" name="book_title" value="<?php echo $book['titolo']; ?>"></td>
-                        <td><input type="text" name="book_author" value="<?php echo $book['autore']; ?>"></td>
-                        <td><input type="text" name="book_isbn" value="<?php echo $book['isbn']; ?>"></td>
-                        <td><input type="text" name="book_genre" value="<?php echo $book['genere']; ?>"></td>
-                        <td><input type="text" name="book_year" value="<?php echo $book['annoPubblicazione']; ?>"></td>
-                        <td><input type="text" name="book_availability" value="<?php echo $book['quantita']; ?>"></td>
-                        <!-- pulsante + che aggiunge un libro alla quantità -->
-                        <form method="POST" action="update_book.php">
+                    <form method="POST" action="update_book.php">
                         <td>
+                            <input type="text" name="book_title" value="<?php echo $book['titolo']; ?>"></td>
+                            <input type="text" name="book_author" value="<?php echo $book['autore']; ?>"></td>
+                            <input type="text" name="book_isbn" value="<?php echo $book['isbn']; ?>"></td>
+                            <input type="text" name="book_genre" value="<?php echo $book['genere']; ?>"></td>
+                            <input type="text" name="book_year" value="<?php echo $book['annoPubblicazione']; ?>"></td>
+                            <input type="text" name="book_availability" value="<?php echo $book['quantita']; ?>"></td>
                             <input type="hidden" name="book_id" value="<?php echo $book['id']; ?>">
                             <input type="submit" name="increment" value="+">
                             <input type="submit" name="decrement" value="-">
                             <td><input type="text" name="book_description" value="<?php echo $book['descrizione']; ?>"></td>
                         <td>
-                        </form>
-                            <input type="hidden" name="book_id" value="<?php echo $book['id']; ?>">
-                            <input type="submit" value="Aggiorna" onclick="return confirmDelete()">
-                        </td>
-                        </td>
-                        </form>
-                        </tr>
-                        <?php } ?>
-                        </table>
                     </form>
                 </tr>
         </table>
--- update_book.php
+++ update_book.php
@@ -2,6 +2,7 @@
 // update_book.php
 $db = mysqli_connect('localhost', 'phpmyadmin', 'ciaone11', 'biblioteca');
 
+//gestione aggiornamento quantità
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $bookId = $_POST['book_id'];
     $query = "SELECT quantita FROM libri WHERE id = '$bookId'";
@@ -23,4 +24,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header('Location: dashboard_admin.php'); 
     exit();
 }
+
+//gestione aggiornamento descrizione
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bookId = $_POST['book_id'];
+    $query = "SELECT descrizione FROM libri WHERE id = '$bookId'";
+    $result = mysqli_query($db, $query);
+    $row = mysqli_fetch_assoc($result);
+    $currentDescription = $row['descrizione'];
+
+    if (isset($_POST['updateDescription'])) {
+        $newDescription = $_POST['newDescription'];
+    } else {
+
+    }
+
+    $updateQuery = "UPDATE libri SET descrizione = '$newDescription' WHERE id = '$bookId'";
+    mysqli_query($db, $updateQuery);
+
+    header('Location: dashboard_admin.php'); 
+    exit();
+}
+
+//gestione aggiornamento autore
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bookId = $_POST['book_id'];
+    $query = "SELECT autore FROM libri WHERE id = '$bookId'";
+    $result = mysqli_query($db, $query);
+    $row = mysqli_fetch_assoc($result);
+    $currentAuthor = $row['autore'];
+
+    if (isset($_POST['updateAuthor'])) {
+        $newAuthor = $_POST['newAuthor'];
+    } else {
+
+    }
+
+    $updateQuery = "UPDATE libri SET autore = '$newAuthor' WHERE id = '$bookId'";
+    mysqli_query($db, $updateQuery);
+
+    header('Location: dashboard_admin.php'); 
+    exit();
+}
+
+//gestione aggiornamento titolo
 ?>
