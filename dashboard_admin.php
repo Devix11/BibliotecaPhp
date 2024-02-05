b55c7e64eb34d35ddb964c1e314a9c42ece78de5
F3yor <f3yor2005@gmail.com>
2024-02-05 09:33:59 +0100

    Update dashboard_admin.php

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -95,12 +95,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php foreach ($books as $book) { ?>
         <tr>
             <table class="w-auto table-auto flex items-center border-b border-teal-500 py-2">
-            <form method="POST" action="update_book.php" class="w-auto max-w-sm">
+            <form method="POST" action="update_book.php" class="w-auto inline-block">
                 <td><?php echo $book['id']; ?></td>
                 <td><input type="hidden" name="book_id" value="<?php echo $book['id']; ?>"></td>
-                <td><input class="w-auto appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newTitle" value="<?php echo $book['titolo']; ?>"></td>
-                <td><input class="w-auto appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newAuthor" value="<?php echo $book['autore']; ?>"></td>
-                <td><input class="w-auto appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newIsbn" value="<?php echo $book['isbn']; ?>"></td>
+                <td><input class="w-auto inline-block appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newTitle" value="<?php echo $book['titolo']; ?>"></td>
+                <td><input class="w-auto inline-block appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newAuthor" value="<?php echo $book['autore']; ?>"></td>
+                <td><input class="w-auto inline-block appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newIsbn" value="<?php echo $book['isbn']; ?>"></td>
                 <td><input class="w-auto appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newGenre" value="<?php echo $book['genere']; ?>"></td>
                 <td><input class="w-auto appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newYear" value="<?php echo $book['annoPubblicazione']; ?>"></td>
                 <td><input class="w-auto appearance-none bg-gray border-none text-gray-700 mr-3 py-1 leading-tight focus:text-red-600" type="text" name="newQuantity" value="<?php echo $book['quantita']; ?>"></td>
